247c5a7a2df8eb1223e0f97a48223497204b11fb
Rarted2 <118175629+Rarted2@users.noreply.github.com>
2025-10-29 09:36:24 +0100

    pruebas con tuplas

--- UCC/pruebas.py
+++ UCC/pruebas.py
@@ -1,2 +1,13 @@
-frase = "hola que tal"
-print(frase[::-1])
+# Añade un número introducido por el usuario al final de una tupla dada
+tupla = (1,2,3,4,5)
+num = int(input("Introduce un numero: "))
+
+
+# Convertir la tupla a lista para poder modificarla
+lista = list(tupla)
+lista.append(num)
+tupla = tuple(lista)
+
+
+# Mostrar la tupla actualizada
+print(tupla)
