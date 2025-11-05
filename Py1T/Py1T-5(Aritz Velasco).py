06a291cee63469040db4f7a3ddbdcd4d15fee3fc
Rarted2 <118175629+Rarted2@users.noreply.github.com>
2025-11-05 08:50:51 +0100

    Update Py1T-5(Aritz Velasco).py

--- Py1T/Py1T-5(Aritz Velasco).py
+++ Py1T/Py1T-5(Aritz Velasco).py
@@ -4,13 +4,13 @@ frase = input("Introduce una frase: ")
 # Pedimos al usuario que introduzca una vocal
 vocal = input("Introduce una vocal: ")
 
-
 # Verificamos que la entrada sea una sola vocal
 while len(vocal) != 1 or vocal.lower() not in "aeiou":
     print("Error: Debes introducir una sola vocal (a, e, i, o, u).")
     vocal = input("Introduce una vocal: ")
 
+for letra in "aeiou":
+    frase = frase.replace(letra.lower(), vocal.upper()) # Reemplazamos la vocal en minúscula por la vocal nueva en mayúscula
+    frase = frase.replace(letra.upper(), vocal.upper()) # Reemplazamos la vocal en mayúscula por la vocal nueva en mayúscula
 
-# Reemplazamos todas las apariciones de la vocal por su versión en mayúscula
-frase_modificada = frase.replace(vocal.lower(), vocal.upper())
-print("Frase modificada:", frase_modificada)
+print("Frase modificada:", frase)
